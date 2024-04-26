c53cd834df5fe96a9b4aeafbc44fbfae6e9e4d61
Hath <Hath@Hath.com>
2024-04-26 09:02:39 -0300

    nice stuff

--- events/ready.js
+++ events/ready.js
@@ -1,24 +1,29 @@
 import fs from 'fs';
+import { ActivityType } from 'discord.js';
 
 const once = true;
 const name = 'ready';
 
 async function invoke(client) {
-	const commands = fs
-		.readdirSync('./events/commands')
-		.filter((file) => file.endsWith('.js'))
-		.map((file) => file.slice(0, -3));
+    const commands = fs
+        .readdirSync('./events/commands')
+        .filter((file) => file.endsWith('.js'))
+        .map((file) => file.slice(0, -3));
 
-	const commandsArray = [];
+    const commandsArray = [];
 
-	for (let command of commands) {
-		const commandFile = await import(`#commands/${command}`);
-		commandsArray.push(commandFile.create());
-	}
+    for (let command of commands) {
+        const commandFile = await import(`#commands/${command}`);
+        commandsArray.push(commandFile.create());
+    }
 
-	client.application.commands.set(commandsArray);
+    client.application.commands.set(commandsArray);
+    await client.user.setActivity("Bloom opening May 1", {
+        type: ActivityType.Streaming,
+        url: "https://twitch.tv/bloom"
+    });
 
-	console.log(`Successfully logged in as ${client.user.tag}!`);
+    console.log(`Successfully logged in as ${client.user.tag}!`);
 }
 
 export { once, name, invoke };
